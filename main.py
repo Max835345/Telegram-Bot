5eaac8488c27bdda32620af047f091973f2d76e4
Max <79370059+Max835345@users.noreply.github.com>
2023-01-30 20:57:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import moviepy.editor
+import os
 from pathlib import Path
 from pytube import YouTube
 
@@ -6,9 +7,10 @@ link = input()
 name = input()
 yt = YouTube(link)
 streams = yt.streams
-video = streams.filter(progressive=True).desc().first().download(filename=f"{name}.mp4")
+vd = streams.filter(progressive=True).desc().first().download(filename=f"{name}.mp4")
 
 video_file = Path(f'{name}.mp4')
 video = moviepy.editor.VideoFileClip(f'{video_file}')
 audio = video.audio
 audio.write_audiofile(f'{video_file}.mp3')
+os.rename(f'{name}.mp4.mp3',f'{name}.mp3')
